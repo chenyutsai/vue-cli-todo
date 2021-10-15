b67971ced0b4e62ec870989fe5e027d902a91c31
chenyutsai <redren7@gmail.com>
2021-10-15 11:50:07 +0800

    TodoList finish

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <li @click="handleDelete">{{ content }}</li>
+    </div>
+</template>
+
+<script>
+export default {
+    // 接收父組件參數
+    props: ['content','index'],
+    methods: {
+        handleDelete () {
+            // 發射訊息給父組件，並將 index 索引值帶過去
+            this.$emit('delete', this.index)
+        }
+    }
+}
+</script>
+
+<style scoped>
+</style>
